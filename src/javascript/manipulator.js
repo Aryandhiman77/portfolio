d161d125e6daa782c185362d88e2da55a28eadf8
Aryandhiman77 <aryandhiman015@gmail.com>
2025-07-15 21:59:53 +0530

    clean ups

--- src/javascript/manipulator.js
+++ src/javascript/manipulator.js
@@ -154,6 +154,8 @@ const automaticallyDarkinEvening = () => {
   if (isEvening) {
     bodyClasses.add("darkmode");
     document.getElementById("invertImage").classList.add("invert");
+    document.getElementById("modelDark").classList.add("bg-black");
+  document.getElementById("modelDark").classList.add("formbg");
     changeToggleIcon();
   }
 };
